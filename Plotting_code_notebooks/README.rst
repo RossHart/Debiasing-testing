7eb61170a6cb60aec4ae52997a46adddec2ffb18
RossHart <ross.hart@nottingham.ac.uk>
2015-09-25 11:48:12 +0100

    Added some stuff.

---
+++ Plotting_code_notebooks/README.rst
@@ -0,0 +1,1 @@
+histograms.ipynb,fractional_plots.ipynb,binned_images.ipynb + contours.ipynb should have all of the plots.
---
+++ Plotting_code_notebooks/params.py
@@ -0,0 +1,15 @@
+gz_dir =  "../../fits/" # Directory with debiased and raw data.
+
+#filename = "Volume_limited_sample_Baldry_w_bd.fits" # FITS file of the 
+# volume-limited sample.
+filename = "data_vl.fits"
+
+#deb_vals = "debiased.npy" # Numpy table containing the debiased values
+deb_vals = "debiased_vl_t11_arms_number.fits"
+#---------------------------------------------------------------------
+
+sf_dir = "../../smpy-ross/" # smpy output file directory
+
+sf_out = "par.csv" # smpy magnitudes
+
+sf_mags = "smpy_synmag_output.mags.npy" # output name
